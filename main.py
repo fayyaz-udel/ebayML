3650cfd7b8213925e3b288518c471766bf1cfcf0
hamed <fayy@udel.edu>
2021-10-02 17:17:15 -0400

    keras

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import numpy as np
-from sklearn.linear_model import LogisticRegression, SGDRegressor
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-
-from preprocessing import preprocess
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.model_selection import train_test_split
 import logging
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers.experimental import preprocessing
+
+from model import build_and_compile_model, train_model
+from preprocessing import preprocess
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -14,17 +13,24 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 
 X, x_quiz, y = preprocess("./data/train.tsv", "./data/quiz.tsv", False)
-
-y.to_csv("fsdfse.csv")
-logging.info("20")
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.1, random_state=42)
-logging.info("21")
-#LogisticRegression(random_state=0)  #
-#reg = make_pipeline(StandardScaler(),SGDRegressor(max_iter=1000, tol=1e-3, verbose=2), verbose=2)
-reg = GradientBoostingRegressor(verbose=2)
-logging.info("22")
-reg.fit(X_train, y_train)
-logging.info("23")
-np.savetxt("./data/quiz_result.csv", reg.predict(x_quiz), delimiter=",")
+
+normalizer = preprocessing.Normalization(axis=-1)
+normalizer.adapt(np.array(X_train))
+
+model = build_and_compile_model()
+history = train_model(model, X_train, y_train)
+
+# np.savetxt("./data/quiz_result.csv", reg.predict(x_quiz), delimiter=",")
 logging.info("finished")
+
+# logging.info("20")
+
+# logging.info("21")
+# #LogisticRegression(random_state=0)  #
+# #reg = make_pipeline(StandardScaler(),SGDRegressor(max_iter=1000, tol=1e-3, verbose=2), verbose=2)
+# reg = GradientBoostingRegressor(verbose=2)
+# logging.info("22")
+# reg.fit(X_train, y_train)
+# logging.info("23")
---
+++ model.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+
+
+def build_and_compile_model(norm):
+    model = tf.keras.Sequential([
+        norm,
+        layers.Dense(128, activation='relu'),
+        layers.Dense(256, activation='relu'),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(1)
+    ])
+
+    model.compile(loss='mean_absolute_error',
+                  optimizer=tf.keras.optimizers.Adam(0.001))
+    return model
+
+
+def train_model(dnn_model, train_features, train_labels):
+    history = dnn_model.fit(
+        train_features, train_labels,
+        validation_split=0.2,
+        verbose=2, epochs=100)
+
+    return history
