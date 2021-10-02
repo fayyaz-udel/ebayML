dc935441e2eb6c3f243b8c32f0069d7f830d8469
hamed <fayy@udel.edu>
2021-10-02 15:26:04 -0400

    postprocess

--- postprocessing.py
+++ postprocessing.py
@@ -1,5 +1,21 @@
+import pandas as pd
+import csv
 
 
 def calculate_delivery_date():
+    quiz_label = pd.read_csv("./data/quiz_result.csv", header=None).round()[0].to_list()
+    quiz_data = pd.read_csv("./data/quiz.tsv", sep="\t")
+    quiz_data = pd.to_datetime(quiz_data["payment_datetime"].str.slice(0, 10))
+    out_file = open('./output.tsv', 'w+', newline='')
+    tsv_writer = csv.writer(out_file, delimiter='\t')
 
+    for index, value in quiz_data.items():
+        tsv_writer.writerow([str(15000001 + index), str(value + pd.Timedelta(days=quiz_label[index]))[:10]])
+        if index % 10000 == 0:
+            print(index)
+    out_file.flush()
+    out_file.close()
     return None
+
+
+calculate_delivery_date()
